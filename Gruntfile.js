01a511d0a9fda475a768859284678557758523d1
Scott Nonnenberg <scott@nonnenberg.com>
2013-11-14 15:37:27 -0800

    reducing duplication for grunt-requirejs setup
    the optimize function makes it really easy to generate requirejs-dependent and standalone versions of your files, each with minified/unminified versions.
    
    Thinking that I'll want to push this into thehelp-project...

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,44 +6,34 @@
 var _ = require('lodash');
 var GruntConfig = require('thehelp-project').GruntConfig;
 
-var optimize = function(grunt, options) {
+var optimize = function(grunt, name, empty, standalone, options) {
   var requirejsMin = _.cloneDeep(options);
-  requirejsMin.out = 'dist/requirejs/thehelp-test.min.js';
-  requirejsMin.paths.winston = 'empty:';
-  requirejsMin.paths.util = 'empty:';
-  grunt.config('requirejs.requirejs-min.options', requirejsMin);
+  requirejsMin.name = name;
+  requirejsMin.out = 'dist/requirejs/' + name + '.min.js';
+  if (empty) {
+    _.forEach(empty, function(module) {
+      requirejsMin.paths[module] = 'empty:';
+    });
+  }
+  grunt.config('requirejs.' + name + '-requirejs-min.options', requirejsMin);
 
   var requirejs = _.cloneDeep(requirejsMin);
   requirejs.optimize = 'none';
-  requirejs.out = 'dist/requirejs/thehelp-test.js';
-  grunt.config('requirejs.requirejs.options', requirejs);
-};
+  requirejs.out = 'dist/requirejs/' + name + '.js';
+  grunt.config('requirejs.' + name + '-requirejs.options', requirejs);
 
-var optimizeStandalone = function(grunt, options) {
-  var standaloneMin = _.cloneDeep(options);
-  standaloneMin.almond = true;
-  standaloneMin.out = 'dist/standalone/thehelp-test.min.js';
-  grunt.config('requirejs.standalone-min.options', standaloneMin);
+  if (standalone) {
+    var standaloneMin = _.cloneDeep(options);
+    standaloneMin.name = name;
+    standaloneMin.almond = true;
+    standaloneMin.out = 'dist/standalone/' + name + '.min.js';
+    grunt.config('requirejs.' + name + '-standalone-min.options', standaloneMin);
 
-  var standalone = _.cloneDeep(standaloneMin);
-  standalone.optimize = 'none';
-  standalone.out = 'dist/standalone/thehelp-test.js';
-  grunt.config('requirejs.standalone.options', standalone);
-};
-
-var optimizeCoverage = function(grunt, options) {
-  var coverageMin = _.cloneDeep(options);
-  coverageMin.out = 'dist/requirejs/thehelp-test-coverage.min.js';
-  coverageMin.name = 'thehelp-test-coverage';
-  coverageMin.paths.winston = 'empty:';
-  coverageMin.paths.util = 'empty:';
-  coverageMin.paths.mocha = 'empty:';
-  grunt.config('requirejs.coverage-min.options', coverageMin);
-
-  var coverage = _.cloneDeep(coverageMin);
-  coverage.optimize = 'none';
-  coverage.out = 'dist/requirejs/thehelp-test-coverage.js';
-  grunt.config('requirejs.coverage.options', coverage);
+    var standalone = _.cloneDeep(standaloneMin);
+    standalone.optimize = 'none';
+    standalone.out = 'dist/standalone/' + name + '.js';
+    grunt.config('requirejs.' + name + '-standalone.options', standalone);
+  }
 };
 
 module.exports = function(grunt) {
@@ -53,9 +43,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-requirejs');
   var options = require('./src/client/config');
-  optimize(grunt, options);
-  optimizeStandalone(grunt, options);
-  optimizeCoverage(grunt, options);
+  optimize(grunt, 'thehelp-test', ['winston', 'util'], true, options);
+  optimize(grunt, 'thehelp-test-coverage', ['winston', 'util', 'mocha'], false, options);
 
   grunt.registerTask('dist', ['requirejs']);
   grunt.registerTask('default', ['test', 'staticanalysis', 'doc', 'dist']);
