e9acd386b8cbf15946c1be3b1afff218fcd1f4a9
Scott Nonnenberg <scott@nonnenberg.com>
2013-11-15 16:57:49 -0800

    manual updates to dependencies
    These three changes are required to get one concatenated file to work properly. Didn't want to spend any more time to get things working in bower_install.sh - damn sed/bash escaping is a total pain in the ass.
    
    We do make one change inside bower_install - setting the blanket option 'existingRequireJS'

--- bower_install.sh
+++ bower_install.sh
@@ -5,13 +5,21 @@ LIB=lib/vendor/
 
 echo "Blanket (concatenated from node module; browserify for its falafel dependency)"
 pushd node_modules/blanket/node_modules/falafel > /dev/null
-browserify --entry index.js --standalone index.js --outfile ../../../../lib/vendor/falafel.js
+browserify --entry index.js --standalone falafel --outfile ../../../../lib/vendor/falafel.js
 popd > /dev/null
+echo "     For now, manually need to remove the UMD stuff from esprima so we don't confuse r.js optimization."
+
 cp node_modules/blanket/src/blanket.js ${LIB}blanket.js
 cat node_modules/blanket/src/blanket_browser.js >> ${LIB}blanket.js
 cat node_modules/blanket/src/adapters/mocha-blanket.js >> ${LIB}blanket.js
 cat node_modules/blanket/src/qunit/reporter.js >> ${LIB}blanket.js
-cp node_modules/blanket/src/blanketRequire.js ${LIB}blanket-require.js
+echo "     For now, manually need to remove the if (!blanket.options(\"existingRequireJS\") statement (call oldRun() always)"
+
+# setting this option in the file itself, because it loads at file load time
+#  unlike a normal AMD module
+echo "blanket.options('existingRequireJS', true);" > ${LIB}blanket-require.js
+echo "" >> ${LIB}blanket-require.js
+cat node_modules/blanket/src/blanketRequire.js >> ${LIB}blanket-require.js
 
 echo "Chai"
 cp ${BOWER}/chai/chai.js ${LIB}
